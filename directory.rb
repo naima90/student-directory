85d92abdf0e263011a8eb43d809ea7d576071f63
Naima <naimaosman203@gmail.com>
2021-12-04 16:45:06 +0000

    refactoring code by adding more methods

--- directory.rb
+++ directory.rb
@@ -1,21 +1,51 @@
+@students = [] #an empty array accessible to all methods
 # asking for user input
 def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # an empty array to store names
-  students = []
   # gets first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the students hash to the array 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  # 1. print the menu and ask user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else 
+    puts "I don't know what you mean, try again"
+  end
 end
 
 #prints the header
@@ -25,19 +55,16 @@ def print_header
 end
 
 #prints the list of students
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #finally, we print the total number of students
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 #calling the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
