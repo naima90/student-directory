67b3aa2442428c241711b7338e3629bfdbd3ac11
Naima <naimaosman203@gmail.com>
2021-12-05 12:44:59 +0000

    taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -1,46 +1,4 @@
 @students = [] #an empty array accessible to all methods
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end  
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-# asking for user input
-def input_students 
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # gets first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the students hash to the array 
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
 
 def print_menu
   # 1. print the menu and ask user what to do
@@ -48,13 +6,14 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list of students.csv"
-  puts "9. Exit"
+  puts "9. Exit" #9 because we'll be adding more items
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -68,29 +27,81 @@ def process(selection)
   when "4"
     load_students
   when "9"
-    exit
+    exit # this will cause the program to terminate
   else 
-    puts "I don't know what you mean, try again"
+    puts "I don't know what you meant, try again"
   end
 end
 
-#prints the header
-def print_header
-  puts "The students of my cohort at Makers"
-  puts "-------------"
+ # asking for user input
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # gets first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the students hash to the array 
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
 end
 
-#prints the list of students
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+# prints a list of students
 def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-
-#finally, we print the total number of students
+# prints total number of students
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-#calling the methods
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end 
+
+def try_load_students
+  filename =  ARGV.first #first argument from the command line
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end 
+
+try_load_students
 interactive_menu
